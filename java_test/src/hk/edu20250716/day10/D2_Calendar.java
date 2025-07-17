02b0a89c21781f193c969e055801494178ba68a3
bjkang@bjkang <kang99king3@naver.com>
2025-07-17 10:49:36 +0900

    Calendar API 추가

--- java_test/src/hk/edu20250716/day10/D2_Calendar.java
+++ java_test/src/hk/edu20250716/day10/D2_Calendar.java
@@ -1,5 +1,7 @@
 package hk.edu20250716.day10;
 
+import java.util.Calendar;
+
 public class D2_Calendar {
 	//윤년일때 달의 마지막 날
 	private static final int[] leap= {31,29,31,30,31,30,31,31,30,31,30,31};
@@ -67,19 +69,57 @@ public class D2_Calendar {
 		}
 		//날짜를 출력
 		for (int i = 1; i <= lastDay(year, month); i++) {
-			System.out.print(i+"\t");
+			System.out.print(i+"\t");//날짜 출력
 			//코드작성: 토요일을 확인하는 조건을 만들어 줄바꿈을 실행하는 코드 작성
-			
+			if((i+dayOfWeek)%7==0) {//토요일이 되는 조건:(현재날짜+공백수)%7==0
+				System.out.println();
+			}
 		}
 	}
 	
 	public static void main(String[] args) {
 		D2_Calendar cal=new D2_Calendar();
-		cal.calendarPrint(2025, 7);
+//		cal.calendarPrint(2025, 8);
 		
 		//실습: 1. 2025년 1월~12월까지 출력하기
+		for (int i = 1; i <= 12; i++) {
+			cal.calendarPrint(2025, i);
+			System.out.println("\n");
+		}
 		//     2. 나의 살아온 날 구하기
+		int a=cal.dates(2025, 7, 17);
+		int birthDay=cal.dates(2003, 7, 10);
+		System.out.println("나의 살아온 날은? "+(a-birthDay)+"일입니다.");
+		
 		//     3. Calendar API사용해서 구현해보기
+		for (int i = 1; i <= 12; i++) {
+			cal.calendarApiPrint(2025, i);			
+		}
+	}
+	
+	public void calendarApiPrint(int year, int month) {
+		//Calendar 추상클래스: new 객체생성 X --> 메서드를 통해 객체를 얻어온다. 
+		Calendar cal = Calendar.getInstance();//객체생성하면 오늘 날짜기준
+//		System.out.println(cal.get(Calendar.DATE));
+		
+		//Cal API에서는 월을 나타낼때 0~11월로 관리함
+		cal.set(year,month-1, 1);//특정 날짜로 셋팅
+		//해당 달의 마지막 날 가져오기: 1~31일 중 가장 큰값
+		int lastDay=cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		int dayOfWeek=cal.get(Calendar.DAY_OF_WEEK);//요일: 1~7로 관리
+//		System.out.println(dayOfWeek);// dayOfWeek-1=공백수
+		System.out.println(year+"년\t"+month+"월");
+		System.out.println("일\t월\t화\t수\t목\t금\t토");
+		for (int i = 0; i < dayOfWeek-1; i++) {
+			System.out.print("\t");
+		}
+		for (int i = 1; i <= lastDay; i++) {
+			System.out.print(i+"\t");
+			if((i+dayOfWeek-1)%7==0) {
+				System.out.println();
+			}
+		}
+		System.out.println("\n");
 	}
 }
 
