1c0dc9c821dab1d1600ab372752f8167e94419a0
bjkang@bjkang <kang99king3@naver.com>
2025-07-21 11:38:19 +0900

    4배수마방진추가

--- java_test/src/hk/edu20250717/day11/D3_MagicSquareMain.java
+++ java_test/src/hk/edu20250717/day11/D3_MagicSquareMain.java
@@ -2,10 +2,12 @@ package hk.edu20250717.day11;
 
 import java.util.Arrays;
 
+import hk.edu20250721.day13.D1_EvenMagicSquare;
+
 public class D3_MagicSquareMain {
 
 	public static void main(String[] args) {
-		D3_OddMagicSquare oddMagic=new D3_OddMagicSquare(17);
+		D3_OddMagicSquare oddMagic=new D3_OddMagicSquare(3);
 		oddMagic.make();
 //		int [][] magic=oddMagic.magic;
 //		for (int i = 0; i < magic.length; i++) {
@@ -15,6 +17,11 @@ public class D3_MagicSquareMain {
 //			System.out.println();
 //		}
 		oddMagic.magicPrint();
+		
+		System.out.println("=========짝수마방진(4배수)=============");
+		D1_EvenMagicSquare evenMagic=new D1_EvenMagicSquare(10);
+		evenMagic.magicPrint();
+		
 	}
 }
 
