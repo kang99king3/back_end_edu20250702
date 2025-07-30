eb8eaf82cd79c79320d8f683904de72132ce50ca
bjkang@bjkang <kang99king3@naver.com>
2025-07-30 11:34:42 +0900

    네트워크 코드 및 채팅코드 추가

--- java_test/src/hk/edu20250729/day19/D2_TCPClient.java
+++ java_test/src/hk/edu20250729/day19/D2_TCPClient.java
@@ -14,8 +14,8 @@ public class D2_TCPClient {
 		BufferedReader userIn=null;//사용자가 키보드로 입력하는 메시지를 읽어들일 객체
 		
 		try {
-//			socket=new Socket("192.168.22.2",9595);
-			socket=new Socket("localhost",9595);//(접속하려는 ip주소,포트)
+			socket=new Socket("192.168.22.2",9595);
+//			socket=new Socket("localhost",9595);//(접속하려는 ip주소,포트)
 			System.out.println("Client:Connetion to server...");
 			out=new PrintWriter(socket.getOutputStream(),true);
 			in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -25,7 +25,7 @@ public class D2_TCPClient {
 			while((inputLine=userIn.readLine())!=null) {
 				//소켓으로 출력  out.print(inputLine)->out.println(inputLine)변경
 				//개행문자가 있어야 문장의 종료를 알 수 있음.<-서버쪽에서 메시지의 끝이 어디인지 몰라 계속 대기
-				out.println(inputLine);//
+				out.println(inputLine);
 				System.out.println("서버에서 전달된 메시지:"+in.readLine());
 			}
 		} catch (Exception e) {
