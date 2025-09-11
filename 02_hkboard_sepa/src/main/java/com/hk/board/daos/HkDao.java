0b858d4a848c3ebdebddc403c6d4524f2d9987cf
bjkang@bjkang <kang99king3@naver.com>
2025-09-11 15:54:58 +0900

    상세보기 완료

--- 02_hkboard_sepa/src/main/java/com/hk/board/daos/HkDao.java
+++ 02_hkboard_sepa/src/main/java/com/hk/board/daos/HkDao.java
@@ -80,6 +80,40 @@ public class HkDao extends Database{
 		return count>0?true:false;
 	}
 	
+	//글상세보기: select where절  반환값은 HkDto
+	public HkDto getBoard(int seq) {
+		HkDto dto=new HkDto();
+		
+		Connection conn=null;
+		PreparedStatement psmt=null;
+		ResultSet rs=null;
+		
+		String sql=" SELECT SEQ, ID, TITLE, CONTENT, REGDATE "
+				+ " FROM HKBOARD WHERE SEQ = ? ";
+		
+		try {
+			conn=getConnetion();
+			psmt=conn.prepareStatement(sql);
+			psmt.setInt(1, seq);// seq의 타입이 int형-> setInt()사용
+			rs=psmt.executeQuery();
+			while(rs.next()) {
+				//java  <==  DB : DB에 값들을 java에서 사용할 수 있게 처리
+				// 인덱스 순서는 select절에 작성한 컬럼순서와 일치
+				dto.setSeq(rs.getInt(1));
+				dto.setId(rs.getString(2));
+				dto.setTitle(rs.getString(3));
+				dto.setContent(rs.getString(4));
+				dto.setRegDate(rs.getDate(5));
+				System.out.println(dto);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(rs, psmt, conn);
+		}
+		return dto;
+	}
+	
 }
 
 
