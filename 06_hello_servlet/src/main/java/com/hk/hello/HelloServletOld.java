d574bc641b9aff90b6a9643b92f3747f40d7d787
bjkang@bjkang <kang99king3@naver.com>
2025-09-22 15:52:35 +0900

    서블릿 코드 추가

--- 06_hello_servlet/src/main/java/com/hk/hello/HelloServletOld.java
+++ 06_hello_servlet/src/main/java/com/hk/hello/HelloServletOld.java
@@ -3,6 +3,8 @@ package com.hk.hello;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,15 +25,35 @@ public class HelloServletOld extends HttpServlet {
     	System.out.println("init():최초 한번 실행");
     }
     
+    //ServletConfig객체의 사용
+    // --> init()메서드에서 파라미터를 통해 얻을 수 있다.
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+    	//web.xml에 정의된 init 값을 가져오기
+    	String name=config.getInitParameter("name");
+    	System.out.println("서블릿초기값:"+name);
+    	
+    	//ServletContext객체 얻어오기(JSP에서 scope객체중에 application객체를 의미함)
+    	ServletContext application=config.getServletContext();
+    	application.setAttribute("id", "hk");//객체저장
+    	String id=(String)application.getAttribute("id");
+    	System.out.println("application값가져오기:"+id);
+    	
+    	//-- web.xml에 servletContext 값을 정의하고, 가져올 수 도 있다.
+    	String driver=application.getInitParameter("driver");
+    	System.out.println("application값가져오기:"+driver);
+    	
+    }
+    
     //service(): 요청과 응답에 대한 처리 -> doGet()/doPost()구현함
     //client에서 get방식으로 localhost:8080/06_hello_servlet/HelloServletOld.do?seq=10&id=hk
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("요청주소:"+request.getRequestURI());
 		System.out.println("요청방식:get방식");
 		
-		//인코딩처리
-		request.setCharacterEncoding("utf-8");
-		response.setContentType("text/html;charset=utf-8");
+		//인코딩처리: Filter로 처리하면 별도의 서블릿에 작성할 필요 없음
+//		request.setCharacterEncoding("utf-8");
+//		response.setContentType("text/html;charset=utf-8");
 		
 		//파리미터 받기
 		String param=request.getParameter("param");
