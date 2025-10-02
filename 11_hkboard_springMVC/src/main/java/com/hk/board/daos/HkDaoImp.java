2092b9b86c271a4af3b91468477f9ef70c6e510f
bjkang@bjkang <kang99king3@naver.com>
2025-10-02 11:42:33 +0900

    dao 구현

--- 11_hkboard_springMVC/src/main/java/com/hk/board/daos/HkDaoImp.java
+++ 11_hkboard_springMVC/src/main/java/com/hk/board/daos/HkDaoImp.java
@@ -1,5 +1,56 @@
 package com.hk.board.daos;
 
-public class HkDaoImp {
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.hk.board.dtos.HkDto;
+
+//Dao 클래스: 쿼리를 실행하는 클래스
+@Repository
+public class HkDaoImp implements IHkDao{
+
+	private String namespace="com.hk.board.dao.";
+	
+	//스프링이 객체의 타입을 확인해서 주입해줌
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Override
+	public List<HkDto> getAllList() {
+		return sqlSession.selectList(namespace+"boardlist");
+	}
+
+	@Override
+	public boolean insertBoard(HkDto dto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public HkDto getBoard(int seq) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateBoard(HkDto dto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteBoard(int seq) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mulDel(String[] seqs) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
