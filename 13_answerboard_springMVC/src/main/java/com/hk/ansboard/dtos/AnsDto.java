551cb95b63d5eb77cfc31d277f40011ef13ede28
bjkang@bjkang <kang99king3@naver.com>
2025-10-13 14:08:53 +0900

    13번 임포트

---
+++ 13_answerboard_springMVC/src/main/java/com/hk/ansboard/dtos/AnsDto.java
@@ -0,0 +1,31 @@
+package com.hk.ansboard.dtos;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Data //getter,setter메서드
+@NoArgsConstructor // default 생성자
+@AllArgsConstructor // 전체 필드 초기화 생성자
+@RequiredArgsConstructor //원하는 필드만 초기화 생성자
+@ToString
+public class AnsDto {
+
+	private @NonNull Integer seq;
+	private @NonNull String id;
+	private @NonNull String title;
+	private @NonNull String content;
+	private Date regDate;
+	private int refer;
+	private int step;
+	private int depth;
+	private String readCount;
+	private String delflag;
+}
