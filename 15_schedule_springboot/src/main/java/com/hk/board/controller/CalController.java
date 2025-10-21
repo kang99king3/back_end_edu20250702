d58178d8df20ff6f9db7524da4239ad3b135eee8
bjkang@bjkang <kang99king3@naver.com>
2025-10-21 16:05:23 +0900

    달력 필요값 추가

--- 15_schedule_springboot/src/main/java/com/hk/board/controller/CalController.java
+++ 15_schedule_springboot/src/main/java/com/hk/board/controller/CalController.java
@@ -1,9 +1,16 @@
 package com.hk.board.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hk.board.service.CalServiceImp;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/schedule")
@@ -12,6 +19,9 @@ public class CalController {
 	// board/boardlist.do , board/boarddetail.do
 	// board/* --> 로그인확인
 	
+	@Autowired
+	private CalServiceImp calService;
+
 	// localhost:9090/schedule/
 	@GetMapping("/")
 	public String home() {
@@ -20,8 +30,10 @@ public class CalController {
 	}
 	
 	@GetMapping("/calendar")
-	public String calendar() {
+	public String calendar(HttpServletRequest request) {
 		System.out.println("calendar페이지");
+		//request(요청)객체를 전달하면, 받은쪽에서도 요청처리를 할 수 있다.
+		Map<String, Integer> calMap = calService.makeCalendar(request);
 		return "calboard/calendar";
 	}
 }
